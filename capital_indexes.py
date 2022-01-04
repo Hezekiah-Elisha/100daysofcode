e80851b8d2e2ec882bae1765cafc435350b5a0a2
Derick Pambah <55497449+Derickpambah@users.noreply.github.com>
2022-01-04 16:50:39 +0300

    Solved a python problem from the Python principles site

--- capital_indexes.py
+++ capital_indexes.py
@@ -1,6 +1,6 @@
-def capital_indexes(strng):
+def capital_indexes(strg):
     lst = []
-    for index, value in enumerate(strng):
+    for index, value in enumerate(strg):
         if value >= "A" and value <= "Z":
             lst.append(index)
     return lst
